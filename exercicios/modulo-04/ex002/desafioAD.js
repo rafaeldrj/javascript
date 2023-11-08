6772e286dcb91c1a090ba8de010769104dfd1181
Rafael Gonçalves <rafael.drj@outlook.com>
2023-11-08 19:56:18 -0300

    desafio AD

---
+++ exercicios/modulo-04/ex002/desafioAD.js
@@ -0,0 +1,15 @@
+var caixa = 12
+var carrinho = 49
+
+var teste = () => {
+  if (carrinho < caixa) {
+    const firstBox = !(caixa - carrinho)
+    return caixa - carrinho
+  } else {
+    const unidadesFaltando = caixa - (carrinho % caixa)
+    return unidadesFaltando
+  }
+  
+}
+
+console.log(teste())
