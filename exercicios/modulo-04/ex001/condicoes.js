025ee7854d3b5debae1b1ec73073b6d873f37ea9
Rafael Gonçalves <rafael.drj@outlook.com>
2023-11-08 15:45:48 -0300

    condições simples

--- exercicios/modulo-04/ex001/condicoes.js
+++ exercicios/modulo-04/ex001/condicoes.js
@@ -1,7 +1,9 @@
+
+// Condição simples!
+
 var vel = 60.5
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
 if (vel > 60) {
-  // Condição simples!
   console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
 }
 console.log(`Dirija sempre usando cinto de segurança!`)
