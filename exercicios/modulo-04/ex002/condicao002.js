4da5fc9c60ad38c8114be133c9f556d34021dc98
Rafael Gonçalves <rafael.drj@outlook.com>
2023-11-08 20:21:32 -0300

    condições aninhadas

--- exercicios/modulo-04/ex002/condicao002.js
+++ exercicios/modulo-04/ex002/condicao002.js
@@ -1,12 +1,12 @@
-var agora = new Date();
-var hora = agora.getHours();
-console.log(`Agora são exatamente ${hora} horas.`);
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
 if (hora < 12) {
-    console.log('Bom dia!');
+  console.log("Bom dia!")
 } else if (hora <= 18) {
-    console.log('Boa tarde!');
+  console.log("Boa tarde!")
 } else if (hora <= 23) {
-    console.log('Boa noite!');
+  console.log("Boa noite!")
 } else {
-    console.log('Boa madrugada!');
+  console.log("Boa madrugada!")
 }
