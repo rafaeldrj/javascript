f2bc12c1a403563fc67ad0b3f076d22fcd46cd9d
Rafael Gonçalves <rafael.drj@outlook.com>
2023-11-10 19:32:29 -0300

    condições aninhadas

--- exercicios/modulo-04/ex002/condicao.js
+++ exercicios/modulo-04/ex002/condicao.js
@@ -1,9 +1,9 @@
 var idade = 22
 console.log(`Você tem ${idade} anos.`)
 if (idade < 16) {
-    console.log('Nao vota') 
+  console.log("Nao vota")
 } else if (idade < 18 || idade > 65) {
-        console.log('Voto opcional')
-    } else {
-        console.log('Voto obrigatório')
-    }
+  console.log("Voto opcional")
+} else {
+  console.log("Voto obrigatório")
+}
