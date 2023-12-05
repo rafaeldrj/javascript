a75886135defa6632db798b1fd499e7dfb27016d
Rafael Gonçalves <rafael.drj@outlook.com>
2023-12-05 16:52:03 -0300

    hora do dia

--- desafios/hora-do-dia/script.js
+++ desafios/hora-do-dia/script.js
@@ -7,11 +7,14 @@ function carregar() {
   if (hora >= 0 && hora < 12) {
     // BOM DIA
     img.src = "image/manha.png"
+    document.body.style.background = "#e2cd9f"
   } else if (hora >= 12 && hora < 18) {
     // BOA TARDE
     img.src = "image/tarde.png"
+    document.body.style.background = "#b9846f"
   } else {
     // BOA NOITE
     img.src = "image/noite.png"
+    document.body.style.background = "#515154"
   }
 }
