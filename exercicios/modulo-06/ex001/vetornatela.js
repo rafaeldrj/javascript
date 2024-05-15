aa3ef84a865287ed317caa210615c6d84e83afe3
Rafael Gonçalves <rafael.drj@outlook.com>
2024-05-15 14:38:25 -0300

    vetor simplificado

--- exercicios/modulo-06/ex001/vetornatela.js
+++ exercicios/modulo-06/ex001/vetornatela.js
@@ -1,5 +1,10 @@
-let valores = [3, 6, 7, 2, 9, 1, 11]
+let valores = [3, 6, 7, 2, 9, 1]
+valores.sort()
 
-for(let pos=0; pos<valores.length; pos++) {
-  console.log (`A posicao ${pos} tem o valor ${valores[pos]}`)
+/* for(let pos=0; pos<valores.length; pos++) {
+  console.log (`A posição ${pos} tem o valor ${valores[pos]}`)
+} */
+
+for(let pos in valores) {
+  console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
